3267ec0c624029cd42dd235ff2ff68ccd939ea62
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-11 15:39:21 +0900

    test: replace SSR with react-test-renderer

--- packages/utils-react/src/index.test.tsx
+++ packages/utils-react/src/index.test.tsx
@@ -1,4 +1,4 @@
-import { renderToStaticMarkup } from "react-dom/server";
+import reactTestRenderer from "react-test-renderer";
 import { describe, expect, it } from "vitest";
 import { Compose } from "./compose";
 import { Debug } from "./debug";
@@ -6,10 +6,19 @@ import { Debug } from "./debug";
 describe("Debug", () => {
   it("basic", () => {
     const el = <Debug debug={{ hello: "world" }} />;
-    expect(renderToStaticMarkup(el)).toMatchInlineSnapshot(`
-      "<details><summary>debug</summary><pre>{
-        &quot;hello&quot;: &quot;world&quot;
-      }</pre></details>"
+    expect(render(el)).toMatchInlineSnapshot(`
+      <details>
+        <summary
+          onClick={[Function]}
+        >
+          debug
+        </summary>
+        <pre>
+          {
+        "hello": "world"
+      }
+        </pre>
+      </details>
     `);
   });
 });
@@ -29,8 +38,21 @@ describe("Compose", () => {
         ]}
       />
     );
-    expect(renderToStaticMarkup(el)).toMatchInlineSnapshot(
-      '"<div class=\\"wrapper-1\\"><div class=\\"wrapper-2\\"><button></button></div></div>"'
-    );
+
+    expect(render(el)).toMatchInlineSnapshot(`
+      <div
+        className="wrapper-1"
+      >
+        <div
+          className="wrapper-2"
+        >
+          <button />
+        </div>
+      </div>
+    `);
   });
 });
+
+function render(el: React.ReactElement) {
+  return reactTestRenderer.create(el).toJSON();
+}
