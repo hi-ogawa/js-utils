2bb06684954f64d580614d3a09cfba6c8a364f0c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-26 20:46:50 +0900

    feat: add groupBy/uniqBy

--- packages/utils/src/lodash.ts
+++ packages/utils/src/lodash.ts
@@ -6,6 +6,38 @@ export function sortBy<T>(ls: T[], ...keyFns: ((x: T) => any)[]): T[] {
   return sortByMap(ls, (x) => keyFns.map((f) => f(x)), arrayCompareFn);
 }
 
+export function groupBy<T, K>(ls: T[], f: (x: T) => K): Map<K, T[]> {
+  const result = new Map<K, T[]>();
+  for (const x of ls) {
+    const y = f(x);
+    if (!result.has(y)) {
+      result.set(y, []);
+    }
+    result.get(y)!.push(x);
+  }
+  return result;
+}
+
+export function uniq<T>(ls: T[]): T[] {
+  return Array.from(new Set(ls));
+}
+
+export function uniqBy<T>(ls: T[], f: (x: T) => unknown): T[] {
+  const ys = new Set<unknown>();
+  return ls.filter((x) => {
+    const y = f(x);
+    if (ys.has(y)) {
+      return false;
+    }
+    ys.add(y);
+    return true;
+  });
+}
+
+//
+// internal
+//
+
 function sortByMap<T, V>(
   ls: T[],
   mapFn: (x: T) => V,
