77f62cb5575fd1bc34315dc6324d93f894db3b7a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-01 13:22:49 +0900

    test: add tests

--- packages/utils/src/lodash.test.ts
+++ packages/utils/src/lodash.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "vitest";
-import { range } from "./lodash";
+import { groupBy, partition, range, sortBy, uniqBy } from "./lodash";
 
 describe("range", () => {
-  it("basics", () => {
+  it("basic", () => {
     expect(range(3)).toMatchInlineSnapshot(`
       [
         0,
@@ -17,6 +17,85 @@ describe("range", () => {
         4,
       ]
     `);
-    expect(range(3, 3)).toMatchInlineSnapshot('[]');
+    expect(range(3, 3)).toMatchInlineSnapshot("[]");
+  });
+});
+
+describe("groupBy", () => {
+  it("basic", () => {
+    expect(groupBy(range(8), (x) => x % 3)).toMatchInlineSnapshot(`
+      Map {
+        0 => [
+          0,
+          3,
+          6,
+        ],
+        1 => [
+          1,
+          4,
+          7,
+        ],
+        2 => [
+          2,
+          5,
+        ],
+      }
+    `);
+  });
+});
+
+describe("partition", () => {
+  it("basic", () => {
+    expect(partition(range(8), (x) => x % 3 === 0)).toMatchInlineSnapshot(`
+      [
+        [
+          0,
+          3,
+          6,
+        ],
+        [
+          1,
+          2,
+          4,
+          5,
+          7,
+        ],
+      ]
+    `);
+  });
+});
+
+describe("sortBy", () => {
+  it("basic", () => {
+    expect(
+      sortBy(
+        range(8),
+        (x) => x % 3,
+        (x) => -x
+      )
+    ).toMatchInlineSnapshot(`
+      [
+        6,
+        3,
+        0,
+        7,
+        4,
+        1,
+        5,
+        2,
+      ]
+    `);
+  });
+});
+
+describe("uniqBy", () => {
+  it("basic", () => {
+    expect(uniqBy(range(8), (x) => x % 3)).toMatchInlineSnapshot(`
+      [
+        0,
+        1,
+        2,
+      ]
+    `);
   });
 });
