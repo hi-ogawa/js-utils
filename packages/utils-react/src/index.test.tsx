c844f2ae48f73d9e2425317cd41f8fe26fd1b87a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-12 22:18:45 +0900

    chore: remove unused

--- packages/utils-react/src/index.test.tsx
+++ packages/utils-react/src/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, renderHook, screen } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import { describe, expect, it } from "vitest";
