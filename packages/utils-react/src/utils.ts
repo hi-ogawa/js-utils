5b59de8513f9af288971f1810e90910be046dfeb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-26 09:37:09 +0900

    feat: add useRefCallbackEffect

--- packages/utils-react/src/utils.ts
+++ packages/utils-react/src/utils.ts
@@ -9,7 +9,7 @@ export function usePrevious<T>(value: T): T {
 }
 
 // cf. https://github.com/facebook/react/issues/14099#issuecomment-440013892
-export function useStableRef<T>(value: T) {
+export function useStableRef<T>(value: T): React.MutableRefObject<T> {
   const ref = React.useRef(value);
 
   // silence SSR useLayoutEffect warning until https://github.com/facebook/react/pull/26395
@@ -33,4 +33,22 @@ export function useStableCallback<F extends (...args: any[]) => any>(
 
 export function useRerender() {
   return React.useReducer((prev) => !prev, false)[1];
+}
+
+// use RefCallback by useEffect like api
+export function useRefCallbackEffect<T>(
+  effect: (value: T) => (() => void) | undefined
+): React.RefCallback<T> {
+  const stableEffect = useStableCallback(effect);
+  const destructorRef = React.useRef<() => void>();
+
+  const refCallback: React.RefCallback<T> = (value) => {
+    if (value) {
+      destructorRef.current = stableEffect(value);
+    } else {
+      destructorRef.current?.();
+    }
+  };
+
+  return React.useCallback(refCallback, []);
 }
