570160456075f1d74ab3076bc216eeac4ac40e1b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-07 20:48:25 +0900

    feat: add theme-script

---
+++ packages/utils-experimental/src/theme-script.ts
@@ -0,0 +1,50 @@
+const themeApi = globalThis as unknown as {
+  // user input
+  __themeStorageKey?: string;
+  __themeDefaultTheme?: string;
+  // exposed api
+  __themeSet: (theme: string) => void;
+  __themeGet: () => string;
+};
+
+const key = themeApi.__themeStorageKey ?? "theme-script";
+const defaultTheme = themeApi.__themeDefaultTheme || "system";
+const prefersDarkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+function getTheme() {
+  return window.localStorage.getItem(key) || defaultTheme;
+}
+
+function setTheme(theme: string) {
+  window.localStorage.setItem(key, theme);
+  applyTheme();
+}
+
+function applyTheme() {
+  const theme = getTheme();
+  const derived =
+    theme === "system" ? (prefersDarkQuery.matches ? "dark" : "light") : theme;
+  disableTransitions(() => {
+    document.documentElement.classList.remove("dark", "light");
+    document.documentElement.classList.add(derived);
+  });
+}
+
+// https://paco.me/writing/disable-theme-transitions
+function disableTransitions(callback: () => void) {
+  const el = document.createElement("style");
+  el.innerHTML = "* { transition: none !important; }";
+  document.head.appendChild(el);
+  callback();
+  window.getComputedStyle(document.documentElement).transition; // force repaint
+  document.head.removeChild(el);
+}
+
+function initTheme() {
+  applyTheme();
+  prefersDarkQuery.addEventListener("change", applyTheme);
+  themeApi.__themeGet = getTheme;
+  themeApi.__themeSet = setTheme;
+}
+
+initTheme();
