9f19514fdec936199ab5cc91956c1901e4713006
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-30 14:56:27 +0900

    feat: add query-proxy package (#52)
    * feat: add query-proxy package
    
    * chore: comment
    
    * feat: add TrpcClientReactQueryProxy
    
    * choer: rename "ReactQuery" to "Query"
    
    * test: add trpc typing test

---
+++ packages/query-proxy/src/record.test.ts
@@ -0,0 +1,97 @@
+import { MutationOptions, QueryClient } from "@tanstack/query-core";
+import { describe, expect, it } from "vitest";
+import { FnRecord, createFnRecordQueryProxy } from "./record";
+
+describe(createFnRecordQueryProxy.name, () => {
+  it("example", async () => {
+    //
+    // define "FnRecord service"
+    //
+
+    let counter = 0;
+
+    const fnRecord = {
+      checkId: (id: string) => id === "good",
+
+      getCounter: () => counter,
+
+      updateCounter: (delta: number) => {
+        counter += delta;
+        return counter;
+      },
+    } satisfies FnRecord;
+
+    //
+    // use Service via QueryClient
+    //
+    const fnRecordQuery = createFnRecordQueryProxy(fnRecord);
+
+    // type-check
+    fnRecordQuery.checkId.queryOptions satisfies (id: string) => {
+      queryKey: unknown[];
+      queryFn: (id: string) => Promise<boolean>;
+    };
+
+    fnRecordQuery.updateCounter.mutationOptions satisfies () => {
+      mutationKey: unknown[];
+      mutationFn: (delta: number) => Promise<number>;
+    };
+
+    const queryClient = new QueryClient();
+
+    expect(
+      await queryClient.fetchQuery(fnRecordQuery.checkId.queryOptions("bad"))
+    ).toMatchInlineSnapshot("false");
+
+    expect(
+      await queryClient.fetchQuery(fnRecordQuery.checkId.queryOptions("good"))
+    ).toMatchInlineSnapshot("true");
+
+    expect(
+      queryClient
+        .getQueryCache()
+        .getAll()
+        .map((e) => ({ queryKey: e.queryKey, data: e.state.data }))
+    ).toMatchInlineSnapshot(`
+      [
+        {
+          "data": false,
+          "queryKey": [
+            "checkId",
+            "bad",
+          ],
+        },
+        {
+          "data": true,
+          "queryKey": [
+            "checkId",
+            "good",
+          ],
+        },
+      ]
+    `);
+
+    expect(
+      await queryClient.fetchQuery(fnRecordQuery.getCounter.queryOptions())
+    ).toMatchInlineSnapshot("0");
+
+    expect(
+      await executeMutation(queryClient, {
+        ...fnRecordQuery.updateCounter.mutationOptions(),
+        variables: +1,
+      })
+    ).toMatchInlineSnapshot("1");
+
+    expect(
+      await queryClient.fetchQuery(fnRecordQuery.getCounter.queryOptions())
+    ).toMatchInlineSnapshot("1");
+  });
+});
+
+// https://github.com/TanStack/query/blob/9b48048a61bfc50483e4a7e16fa28ac23626896d/packages/query-core/src/tests/utils.ts#L57-L62
+function executeMutation(
+  queryClient: QueryClient,
+  options: MutationOptions<any, any, any, any>
+) {
+  return queryClient.getMutationCache().build(queryClient, options).execute();
+}
