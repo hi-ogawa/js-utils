47d7a23e45bdc8569e59d7f4806b035f89105345
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-01 09:49:24 +0900

    feat: add tiny-rpc package (#54)
    * feat: add tiny-rpc package
    
    * fix: fix createServerHandler
    
    * chore: readme
    
    * refactor: rename
    
    * test: wip
    
    * ci: build before tsc
    
    * refactor: code gold
    
    * test: add e2e
    
    * feat: sronger type
    
    * chore: release
    
    * chore: readme
    
    * fix(query-proxy): fix infiniteQueryOptions
    
    * chore: readme
    
    * test: demo zodFn
    
    * chore: todo
    
    * feat: export zodFn
    
    * chore: todo
    
    * feat: custom (de)serialization
    
    * chore: tweak typing
    
    * test: zod example
    
    * test: demo request context
    
    * chore: readme
    
    * chore: release

---
+++ packages/tiny-rpc/README.md
@@ -0,0 +1,15 @@
+# tiny-rpc
+
+extremely simple alternative for `tRPC`.
+
+- no "context"
+  - user code can bring own logic by e.g. [`AsyncLocalStorage`](https://nodejs.org/api/async_context.html)
+- no "query/mutation" distinction
+- use general `@hiogawa/query-proxy` for "react-query" integration
+
+## todo
+
+- support `GET` endpoint?
+  - everything is `POST`, which makes it not possible to utilize `GET` based http caching.
+- error handling convention?
+  - https://trpc.io/docs/server/error-handling
---
+++ packages/tiny-rpc/src/index.ts
@@ -0,0 +1,94 @@
+import { tinyassert } from "@hiogawa/utils";
+
+//
+// server
+//
+
+export type TinyRpcRoutes = Record<string, (() => any) | ((input: any) => any)>;
+
+export function createTinyRpcHandler({
+  endpoint,
+  routes,
+  transformer = jsonTransformer,
+}: {
+  endpoint: string;
+  routes: TinyRpcRoutes;
+  transformer?: Transformer;
+}) {
+  return async ({ url, request }: { url: URL; request: Request }) => {
+    tinyassert(url.pathname.startsWith(endpoint));
+    tinyassert(request.method === "POST");
+
+    const path = url.pathname.slice(endpoint.length + 1);
+    const fn = routes[path];
+    tinyassert(fn);
+
+    const requestJson = transformer.deserialize(await request.text());
+    tinyassert(requestJson);
+    tinyassert(typeof requestJson === "object");
+
+    const output = await fn((requestJson as any).input);
+    return new Response(transformer.serialize({ output }));
+  };
+}
+
+//
+// client
+//
+
+type TinyRpcRoutesAsync<R extends TinyRpcRoutes> = {
+  [K in keyof R]: (
+    ...args: Parameters<R[K]>
+  ) => Promise<Awaited<ReturnType<R[K]>>>;
+};
+
+export function createTinyRpcClientProxy<R extends TinyRpcRoutes>({
+  endpoint,
+  transformer = jsonTransformer,
+  headers,
+  fetch: fetchImpl = fetch,
+}: {
+  endpoint: string;
+  transformer?: Transformer;
+  headers?: () => Record<string, string>;
+  fetch?: typeof fetch;
+}): TinyRpcRoutesAsync<R> {
+  return new Proxy(
+    {},
+    {
+      get(_target, path, _receiver) {
+        tinyassert(typeof path === "string");
+
+        return async (input: unknown) => {
+          const url = [endpoint, path].join("/");
+          const response = await fetchImpl(url, {
+            method: "POST",
+            body: transformer.serialize({ input }),
+            headers: headers?.() ?? {},
+          });
+          tinyassert(response.ok);
+
+          const responseJson = transformer.deserialize(await response.text());
+          tinyassert(responseJson);
+          tinyassert(typeof responseJson === "object");
+
+          return (responseJson as any).output;
+        };
+      },
+    }
+  ) as any;
+}
+
+//
+// common
+//
+
+interface Transformer {
+  serialize: (v: unknown) => string;
+  deserialize: (v: string) => unknown;
+}
+
+const jsonTransformer: Transformer = {
+  serialize: JSON.stringify,
+  deserialize: JSON.parse,
+};
