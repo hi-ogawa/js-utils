5374a301dbbdde7de57ffd12037106b6518404d6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-01 09:05:36 +0900

    test: zod example

--- packages/tiny-rpc/src/e2e.test.ts
+++ packages/tiny-rpc/src/e2e.test.ts
@@ -1,3 +1,4 @@
+import { AsyncLocalStorage } from "node:async_hooks";
 import { createServer } from "@hattip/adapter-node";
 import { compose } from "@hattip/compose";
 import { tinyassert } from "@hiogawa/utils";
@@ -14,12 +15,31 @@ import { zodFn } from "./zod";
 // example rpc
 //
 
-// TODO: async context example
-
 function defineExampleRpc() {
+  //
+  // service internal
+  //
   let counter = 0;
 
-  return {
+  //
+  // context
+  //
+  interface RpcContext {
+    request: Request;
+  }
+
+  const contextStorage = new AsyncLocalStorage<RpcContext>();
+
+  function useContext() {
+    const ctx = contextStorage.getStore();
+    tinyassert(ctx);
+    return ctx;
+  }
+
+  //
+  // service api
+  //
+  const routes = {
     // define as a bare function
     checkId: (id: string) => id === "good",
 
@@ -33,7 +53,15 @@ function defineExampleRpc() {
         return counter;
       }
     ),
+
+    // access context (implicit input e.g. request session etc...)
+    checkAuth: () => {
+      const { request } = useContext();
+      return request.headers.get("x-auth") === "good";
+    },
   } satisfies TinyRpcRoutes;
+
+  return { routes, contextStorage };
 }
 
 //
@@ -44,7 +72,8 @@ describe("e2e", () => {
   it("basic", async () => {
     // define example rpc
     const endpoint = "/rpc";
-    const routes = defineExampleRpc();
+    const { routes, contextStorage } = defineExampleRpc();
+    contextStorage;
 
     // server
     const handler = createTinyRpcHandler({ endpoint, routes });
@@ -58,11 +87,20 @@ describe("e2e", () => {
     expect(await client.checkId("bad")).toMatchInlineSnapshot("false");
 
     expect(await client.getCounter()).toMatchInlineSnapshot("0");
+
+    // default value by zod
     expect(await client.incrementCounter({})).toMatchInlineSnapshot("1");
     expect(await client.getCounter()).toMatchInlineSnapshot("1");
+
     expect(await client.incrementCounter({ delta: 2 })).toMatchInlineSnapshot(
       "3"
     );
+    expect(await client.getCounter()).toMatchInlineSnapshot("3");
+
+    // zod validation
+    await expect(
+      client.incrementCounter({ delta: "2" as any })
+    ).rejects.toMatchInlineSnapshot("[Error]");
     expect(await client.getCounter()).toMatchInlineSnapshot("3");
 
     server.close();
