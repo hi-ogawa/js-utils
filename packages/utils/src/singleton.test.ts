a6b8aba45f27a28195d54e72e6367b8a0169fcd4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-21 16:42:30 +0900

    feat: add `Singleton` helper

---
+++ packages/utils/src/singleton.test.ts
@@ -0,0 +1,179 @@
+import { describe, expect, it } from "vitest";
+import { wrapError } from "./result";
+import { Singleton, type SingletonHooks } from "./singleton";
+
+describe(Singleton, () => {
+  it("basic", async () => {
+    const singleton = new Singleton();
+    const logs: any[] = [];
+
+    class App implements SingletonHooks {
+      config = singleton.resolve(Config);
+      database = singleton.resolve(Database);
+
+      constructor() {
+        logs.push("App.constructor");
+      }
+
+      async init() {
+        logs.push("App.init");
+      }
+
+      async deinit() {
+        logs.push("App.deinit");
+      }
+    }
+
+    class Database implements SingletonHooks {
+      config = singleton.resolve(Config);
+
+      constructor() {
+        logs.push("Database.constructor");
+      }
+
+      async init() {
+        logs.push("Database.init");
+      }
+
+      async deinit() {
+        logs.push("Database.deinit");
+      }
+    }
+
+    class Config implements Pick<SingletonHooks, "init"> {
+      constructor() {
+        logs.push("Config.constructor");
+      }
+
+      async init() {
+        logs.push("Config.init");
+      }
+    }
+
+    prettyPrintClasses([App, Database, Config]);
+
+    // resolve top module
+    const app = singleton.resolve(App);
+    app;
+
+    // check internal
+    expect(singleton.stack).toMatchInlineSnapshot("[]");
+    expect(singleton.deps).toMatchInlineSnapshot(`
+      [
+        [
+          "[class App]",
+          "[class Config]",
+        ],
+        [
+          "[class App]",
+          "[class Database]",
+        ],
+        [
+          "[class Database]",
+          "[class Config]",
+        ],
+      ]
+    `);
+    expect(singleton.instances).toMatchInlineSnapshot(`
+      Map {
+        "[class Config]" => Config {},
+        "[class Database]" => Database {
+          "config": Config {},
+        },
+        "[class App]" => App {
+          "config": Config {},
+          "database": Database {
+            "config": Config {},
+          },
+        },
+      }
+    `);
+    expect(singleton.sortDeps()).toMatchInlineSnapshot(`
+      [
+        Config {},
+        Database {
+          "config": Config {},
+        },
+        App {
+          "config": Config {},
+          "database": Database {
+            "config": Config {},
+          },
+        },
+      ]
+    `);
+
+    // check init/deinit calls
+    expect(logs).toMatchInlineSnapshot(`
+      [
+        "Config.constructor",
+        "Database.constructor",
+        "App.constructor",
+      ]
+    `);
+
+    await singleton.init();
+    expect(logs).toMatchInlineSnapshot(`
+      [
+        "Config.constructor",
+        "Database.constructor",
+        "App.constructor",
+        "Config.init",
+        "Database.init",
+        "App.init",
+      ]
+    `);
+
+    await singleton.deinit();
+    expect(logs).toMatchInlineSnapshot(`
+      [
+        "Config.constructor",
+        "Database.constructor",
+        "App.constructor",
+        "Config.init",
+        "Database.init",
+        "App.init",
+        "Database.deinit",
+        "App.deinit",
+      ]
+    `);
+  });
+
+  it("error-cyclic-deps", () => {
+    const singleton = new Singleton();
+
+    class X {
+      config = singleton.resolve(Y);
+    }
+
+    class Y {
+      config = singleton.resolve(Z);
+    }
+
+    class Z {
+      config = singleton.resolve(X);
+    }
+
+    prettyPrintClasses([X, Y, Z]);
+
+    const result = wrapError(() => singleton.resolve(X));
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "ok": false,
+        "value": [Error: Singleton.resolve detected cyclic dependency],
+      }
+    `);
+    expect((result.value as any).cause).toMatchInlineSnapshot('"[class X]"');
+  });
+});
+
+function prettyPrintClasses(klasses: unknown[]) {
+  expect.addSnapshotSerializer({
+    test(val) {
+      return klasses.includes(val);
+    },
+    serialize(val, config, indentation, depth, refs, printer) {
+      return printer(`[class ${val.name}]`, config, indentation, depth, refs);
+    },
+  });
+}
---
+++ packages/utils/src/singleton.ts
@@ -0,0 +1,118 @@
+import { range } from "./lodash";
+import { tinyassert } from "./tinyassert";
+
+// tsyringe-like idea https://github.com/microsoft/tsyringe
+// but with following benefits
+// - extreme simplicity
+// - it doesn't rely on typescript decorator metadata
+// - it provides dependency graph (e.g. to implement per-module async init/deinit hooks)
+
+type InstanceKey = new () => unknown;
+type Instance = unknown;
+
+export interface SingletonHooks {
+  init: () => void | Promise<void>;
+  deinit: () => void | Promise<void>;
+}
+
+export class Singleton {
+  instances = new Map<InstanceKey, Instance>();
+
+  // manage `resolve` call stack to create module dependency graph
+  // which is necessary for init/deinit ordering
+  stack: InstanceKey[] = [];
+  deps: [InstanceKey, InstanceKey][] = [];
+
+  resolve<T>(ctor: new () => T): T {
+    // add dependency
+    const parent = this.stack.at(-1);
+    if (parent) {
+      this.deps.push([parent, ctor]);
+    }
+
+    if (this.stack.includes(ctor)) {
+      throw new Error("Singleton.resolve detected cyclic dependency", {
+        cause: ctor,
+      });
+    }
+
+    // instantiate within new stack
+    this.stack.push(ctor);
+
+    let instance = this.instances.get(ctor) as T;
+    if (!instance) {
+      instance = new ctor();
+      this.instances.set(ctor, instance);
+    }
+
+    this.stack.pop();
+
+    return instance as T;
+  }
+
+  async init(): Promise<void> {
+    for (const instance of this.sortDeps()) {
+      await (instance as Partial<SingletonHooks>).init?.();
+    }
+  }
+
+  async deinit(): Promise<void> {
+    // TODO: shouldn't it be reversed?
+    for (const instance of this.sortDeps()) {
+      await (instance as Partial<SingletonHooks>).deinit?.();
+    }
+  }
+
+  sortDeps(): Instance[] {
+    const keys = [...this.instances.keys()];
+    const sortedKeys = topologicalSort(keys, this.deps);
+    return sortedKeys.map((k) => this.instances.get(k)!);
+  }
+}
+
+//
+// topological sort
+//
+
+function topologicalSort<T>(verts: T[], edges: [T, T][]): T[] {
+  const indexMap = new Map(verts.map((k, i) => [k, i]));
+
+  const n = verts.length;
+  const adj: number[][] = range(n).map(() => []);
+  for (const [k1, k2] of edges) {
+    const i1 = indexMap.get(k1);
+    const i2 = indexMap.get(k2);
+    tinyassert(typeof i1 === "number");
+    tinyassert(typeof i2 === "number");
+    adj[i1].push(i2);
+  }
+
+  const sortedIndices = topologicalSortInner(adj);
+  return sortedIndices.map((i) => verts[i]);
+}
+
+function topologicalSortInner(adj: number[][]): number[] {
+  // dfs + sort by "exit" time
+
+  const n = adj.length;
+  const visited = range(n).map(() => false);
+  const result: number[] = [];
+
+  function dfs(v: number) {
+    visited[v] = true;
+    for (const u of adj[v]) {
+      if (!visited[u]) {
+        dfs(u);
+      }
+    }
+    result.push(v);
+  }
+
+  for (const v of range(n)) {
+    if (!visited[v]) {
+      dfs(v);
+    }
+  }
+
+  return result;
+}
