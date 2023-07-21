9a55c9278e66173e10a004df28848ed79eca019a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-21 13:25:20 +0900

    feat(utils): add `mapToAsyncGenerator` (#69)

--- packages/utils/src/promise.ts
+++ packages/utils/src/promise.ts
@@ -7,3 +7,44 @@ export function newPromiseWithResolvers<T>() {
   });
   return { promise, resolve, reject };
 }
+
+// somewhat more flexible variant of http://bluebirdjs.com/docs/api/promise.map.html
+export async function* mapToAsyncGenerator<T1, T2>(
+  values: Iterable<T1>,
+  f: (value: T1, index: number) => T2 | PromiseLike<T2>,
+  { concurrency }: { concurrency: number }
+): AsyncGenerator<T2> {
+  const pendings: Promise<() => T2>[] = [];
+
+  let index = 0;
+  for (const value of values) {
+    if (pendings.length >= concurrency) {
+      // TODO: find and yield other resolved promises after Promise.race?
+      yield (await Promise.race(pendings))();
+    }
+
+    const promise = (async () => {
+      const result = await f(value, index++);
+      return () => {
+        // synchronously cleanup itself then return
+        pendings.splice(pendings.indexOf(promise), 1);
+        return result;
+      };
+    })();
+    pendings.push(promise);
+  }
+
+  while (pendings.length > 0) {
+    yield (await Promise.race(pendings))();
+  }
+}
+
+export async function arrayFromAsyncGenerator<T>(
+  generator: AsyncGenerator<T>
+): Promise<T[]> {
+  const result: T[] = [];
+  for await (const v of generator) {
+    result.push(v);
+  }
+  return result;
+}
