a7284166b14c5d74765a8654c0225a2cfb93111e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-23 15:05:23 +0900

    feat(theme-script): add vite plugin

--- packages/theme-script/examples/vite/vite.config.ts
+++ packages/theme-script/examples/vite/vite.config.ts
@@ -1,27 +1,13 @@
-import { THEME_SCRIPT } from "@hiogawa/theme-script";
+import { themeScriptPlugin } from "@hiogawa/theme-script/dist/vite";
 import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
-import { Plugin, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [unocss(), react(), injectThemeScriptPlugin()],
+  plugins: [
+    unocss(),
+    react(),
+    themeScriptPlugin({ storageKey: "theme-script-vite:theme" }),
+  ],
   clearScreen: false,
 });
-
-// inject raw script into html head to initialize style as early as possible
-function injectThemeScriptPlugin(): Plugin {
-  return {
-    name: "local:" + injectThemeScriptPlugin.name,
-    transformIndexHtml() {
-      return [
-        {
-          tag: "script",
-          children: `
-            window.THEME_SCRIPT_STORAGE_KEY = "theme-script-vite:theme";
-            ${THEME_SCRIPT}
-          `,
-        },
-      ];
-    },
-  };
-}
