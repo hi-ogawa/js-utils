c6ba0db90f0c35bc48cc6cfc9a60b0e840a5aef2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-06 21:48:53 +0900

    test: fix stack

--- packages/utils/src/error.test.ts
+++ packages/utils/src/error.test.ts
@@ -1,39 +1,43 @@
 import { describe, expect, it } from "vitest";
 import { flattenErrorCauses, formatError } from "./error";
 
+function reduceStacktrace(s: string) {
+  return s.replaceAll(/at .*/g, "at (reducted)");
+}
+
 describe(formatError, () => {
   it("basic", () => {
     const e1 = new Error("e1", { cause: "just-string" });
     const e2 = new Error("e2", { cause: e1 });
 
-    expect(formatError(e2)).toMatchInlineSnapshot(`
+    expect(reduceStacktrace(formatError(e2))).toMatchInlineSnapshot(`
       "
       [41m ERROR [49m e2
 
-      [36m    at /home/hiroshi/code/personal/js-utils/packages/utils/src/error.test.ts:7:16
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:138:13
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:41:26
-          at runTest (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:486:17)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runFiles (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:645:5)
-          at startTests (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:654:3)
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:278:7
-          at withEnv (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:186:5)[39m
+      [36m    at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
 
 
       [41m ERROR:CAUSE [49m e1
 
-      [36m    at /home/hiroshi/code/personal/js-utils/packages/utils/src/error.test.ts:6:16
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:138:13
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:41:26
-          at runTest (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:486:17)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runFiles (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:645:5)
-          at startTests (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:654:3)
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:278:7
-          at withEnv (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:186:5)[39m
+      [36m    at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
 
 
       [41m ERROR:CAUSE:2 [49m just-string
@@ -41,51 +45,53 @@ describe(formatError, () => {
       "
     `);
 
-    expect(formatError(e2, { noCause: true })).toMatchInlineSnapshot(`
+    expect(reduceStacktrace(formatError(e2, { noCause: true })))
+      .toMatchInlineSnapshot(`
       "
       [41m ERROR [49m e2
 
-      [36m    at /home/hiroshi/code/personal/js-utils/packages/utils/src/error.test.ts:7:16
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:138:13
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:41:26
-          at runTest (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:486:17)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runFiles (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:645:5)
-          at startTests (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:654:3)
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:278:7
-          at withEnv (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:186:5)[39m
+      [36m    at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
       "
     `);
 
-    expect(formatError(e2, { noColor: true })).toMatchInlineSnapshot(`
+    expect(reduceStacktrace(formatError(e2, { noColor: true })))
+      .toMatchInlineSnapshot(`
       "
       [ERROR] e2
 
-          at /home/hiroshi/code/personal/js-utils/packages/utils/src/error.test.ts:7:16
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:138:13
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:41:26
-          at runTest (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:486:17)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runFiles (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:645:5)
-          at startTests (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:654:3)
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:278:7
-          at withEnv (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:186:5)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
 
 
       [ERROR:CAUSE] e1
 
-          at /home/hiroshi/code/personal/js-utils/packages/utils/src/error.test.ts:6:16
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:138:13
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:41:26
-          at runTest (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:486:17)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runSuite (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:594:15)
-          at runFiles (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:645:5)
-          at startTests (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/@vitest+runner@0.32.2/node_modules/@vitest/runner/dist/index.js:654:3)
-          at file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:278:7
-          at withEnv (file:///home/hiroshi/code/personal/js-utils/node_modules/.pnpm/vitest@0.32.2/node_modules/vitest/dist/entry.js:186:5)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
+          at (reducted)
 
 
       [ERROR:CAUSE:2] just-string
