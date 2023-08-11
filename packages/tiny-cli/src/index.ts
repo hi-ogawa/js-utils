462cf91d89e819b6ed978d0d19acd649bd72917a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-11 13:09:22 +0900

    chore: rename

--- packages/tiny-cli/src/index.ts
+++ packages/tiny-cli/src/index.ts
@@ -3,3 +3,4 @@ export * from "./presets";
 export * from "./zod";
 export { TinyCliParseError } from "./utils";
 export { type ArgSchema, type ArgSchemaRecord, type TypedArgs } from "./typed";
+export * from "./main";
--- packages/tiny-cli/src/main.ts
+++ packages/tiny-cli/src/main.ts
@@ -1,18 +1,23 @@
-import process from "node:process";
 import { Err, Ok, type Result, formatError } from "@hiogawa/utils";
 import type { TinyCli, TinyCliCommand } from "./cli";
 import { TinyCliParseError } from "./utils";
 
 export async function tinyCliMain(
-  cli: TinyCli | TinyCliCommand<any>
+  cli: TinyCli | TinyCliCommand<any>,
+  opts?: {
+    process?: Pick<(typeof globalThis)["process"], "argv" | "exitCode">;
+    log?: (v: string) => void;
+  }
 ): Promise<Result<unknown, unknown>> {
+  const process = opts?.process ?? globalThis.process;
+  const log = opts?.log ?? globalThis.console.log;
   try {
     const v = await cli.parse(process.argv.slice(2));
     return Ok(v);
   } catch (e) {
-    console.error(formatError(e));
+    log(formatError(e));
     if (e instanceof TinyCliParseError) {
-      console.log("See '--help' for more info");
+      log("See '--help' for more info");
     }
     process.exitCode = 1;
     return Err(e);
