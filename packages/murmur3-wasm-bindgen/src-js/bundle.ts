182f2d80b6755d993a1a75b596dc3c4d29d482ca
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-23 10:14:18 +0900

    chore: comment

--- packages/murmur3-wasm-bindgen/src-js/bundle.ts
+++ packages/murmur3-wasm-bindgen/src-js/bundle.ts
@@ -1,9 +1,9 @@
-import initWasmBindGen from "../pkg/index";
-import wasmSource from "../pkg/index_bg.wasm"; // let tsup/esbuild bundles binary ~ 20KB
+import pkgInit from "../pkg/index";
+import pkgWasm from "../pkg/index_bg.wasm"; // let tsup/esbuild bundle wasm binary ~ 20KB
 
 export async function init() {
-  const wasmModule = await WebAssembly.compile(wasmSource as any as Uint8Array);
-  await initWasmBindGen(wasmModule);
+  const wasmModule = await WebAssembly.compile(pkgWasm as any as Uint8Array);
+  await pkgInit(wasmModule);
 }
 
 // re-export
