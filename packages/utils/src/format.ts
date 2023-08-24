53127ebf30c65a1f784c4232841a452232c3ea81
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-24 13:57:26 +0900

    wip

--- packages/utils/src/format.ts
+++ packages/utils/src/format.ts
@@ -2,15 +2,39 @@
 // https://doc.rust-lang.org/std/fmt/
 // https://docs.python.org/3/library/string.html#format-string-syntax
 
-export function formatString(s: string) {
-  s;
-  // String.raw`Hello`;
-  // ["s", ]
+type FormatPrimitive = string | number | boolean | null | undefined;
+type FormatSpec = string | ParsedFormatSpec;
+type FormatParam = FormatPrimitive | [FormatSpec, FormatPrimitive | unknown];
+
+export function formatString(
+  strings: TemplateStringsArray,
+  ...params: FormatParam[]
+) {
+  strings;
+  params;
+  parseFormatSpec;
+}
+
+// https://doc.rust-lang.org/std/fmt/#syntax
+type ParsedFormatSpec = {
+  fill?: string;
+  align?: "<" | "^" | ">";
+  width?: number;
+  sign?: "+";
+  precision?: number;
+  type?: "b" | "x" | "?";
+};
+
+function parseFormatSpec(spec: string): ParsedFormatSpec {
+  spec;
+  return {};
 }
 
 demo;
 function demo() {
-  const fmt = String.raw;
+  const fmt = formatString;
+
+  String.raw`${[0, 1]}`;
 
   fmt`hello`;
   fmt`hello ${"world"}!`;
@@ -26,9 +50,9 @@ function demo() {
   // b
   //
 
-
   // leading zero padding
-  fmt`${[":04", 2022]}-${[":02", 8]}-${[":02", 24]}:${[":03", 12]}`;
+  fmt`${["02", 13]}:${["02", 5]}:${["02", 8]}.${["03", 12]}`;
+  fmt`${[{ fill: "0", width: 2 }, 8]}.${[{ fill: "0", width: 3 }, 12]}`;
 
   // {}
   fmt`${8}:02`;
