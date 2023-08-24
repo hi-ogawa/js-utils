f3f239ff42366ed7d865323ab0b4d81ad9dfb5c8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-24 14:57:57 +0900

    wip 3

--- packages/import-graph/README.md
+++ packages/import-graph/README.md
@@ -3,3 +3,4 @@
 is it possible to do all analyze based on `es-module-lexer`?
 maybe it's tricky to support "star" import?
 actually it doesn't provide import names? https://github.com/guybedford/es-module-lexer/issues/100
+also, of course, typescript syntax parsing is not directly supported.
--- packages/import-graph/src/index.ts
+++ packages/import-graph/src/index.ts
@@ -1,24 +1,92 @@
+import { tinyassert, typedBoolean } from "@hiogawa/utils";
 import * as esModuleLexer from "es-module-lexer";
-
-async function main() {
-  await esModuleLexer.init;
-}
-
-// star import?
-
-// interface AnalyzeResult {
-//   dependencies
-//   dependents
-// }
 
 interface ImportInfo {
   source: string;
-  // specifier
+  dynamic: boolean;
+  specifiers: string[];
+  specifierDefault: boolean;
+  specifierStar: boolean;
+  position: number;
 }
 
-function analyzeCode({ code }: { path: string; code: string }) {
+interface ExportInfo {
+  name: string; // include "default"
+  position: number;
+}
+
+export function analyzeCode({ code }: { code: string }) {
   const [esmImports, esmExports] = esModuleLexer.parse(code);
-  esmImports[0].a;
+  return {
+    importInfos: esmImports
+      .map((e) => parseLexerImportResult(code, e))
+      .filter(typedBoolean),
+    exportInfos: esmExports.map((e) => parseLexerExportResult(code, e)),
+  };
 }
 
-main();
+function parseLexerImportResult(
+  code: string,
+  result: esModuleLexer.ImportSpecifier
+): ImportInfo | undefined {
+  // skip import.meta
+  if (result.d === -2) {
+    return;
+  }
+
+  // skip if lexer cannot resolve source (e.g. import("./" + "ab.js"))
+  const source = result.n;
+  if (!source) {
+    return;
+  }
+
+  // dynamic import
+  if (result.d >= 0) {
+    return {
+      source,
+      position: result.ss,
+      dynamic: true,
+      specifiers: [],
+      specifierDefault: false,
+      specifierStar: false,
+    };
+  }
+
+  // parse specifiers for static import
+  tinyassert(result.d === -1);
+  const codeImportDecl = code.slice(result.ss, result.se);
+  const declInfo = parseImportDecl(codeImportDecl);
+
+  return {
+    source,
+    position: result.ss,
+    dynamic: false,
+    ...declInfo,
+  };
+}
+
+function parseImportDecl(
+  code: string
+): Pick<ImportInfo, "specifiers" | "specifierDefault" | "specifierStar"> {
+  // import * as someStar from ...
+  // import { someName, ... } from ...
+  // import { someName as someRename, ... } from ...
+  // import someDefault from ...
+  // import someDefault, { andSomeName, ... } from ...
+
+  // TODO
+
+  code;
+  return {
+    specifiers: [],
+    specifierDefault: false,
+    specifierStar: false,
+  };
+}
+
+function parseLexerExportResult(
+  _code: string,
+  result: esModuleLexer.ExportSpecifier
+): ExportInfo {
+  return { name: result.n, position: result.s };
+}
