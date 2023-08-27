a06fb482ce388fce2f8b1d1a24978e882430b9b0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-27 10:49:28 +0900

    chore: comment

--- misc/generate-tsconfig.sh
+++ misc/generate-tsconfig.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# usage
+#   bash misc/generate-tsconfig.sh > tsconfig.json
+
 pnpm ls --filter './packages/**' --depth -1 --json \
   | jq -r --argjson PWD_LEN "${#PWD}" '{ include: [], references: map({ path: .path[$PWD_LEN + 1:] }) }'
