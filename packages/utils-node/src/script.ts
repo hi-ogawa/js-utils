80b8792d02a2a9b10a51e4703f9adbaf8b83d4b4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-27 12:25:13 +0900

    feat: add `utils-node` package

---
+++ packages/utils-node/src/script.ts
@@ -0,0 +1,101 @@
+import { Buffer } from "node:buffer";
+import {
+  type ChildProcess,
+  type SpawnOptions,
+  spawn,
+} from "node:child_process";
+import { zip } from "@hiogawa/utils";
+
+// too simple but maybe useful enough version of
+// https://github.com/google/zx
+// https://github.com/sindresorhus/execa/blob/f4b8b3ab601c94d1503f1010822952758dcc6350/docs/scripts.md
+
+type ScriptParam = string | number;
+
+type HelperOptions = {
+  noTrim?: boolean;
+  verbose?: boolean;
+};
+
+const defaultSpawnOptions: SpawnOptions = {
+  shell: true,
+  stdio: ["ignore", "pipe", "pipe"],
+};
+
+export const $ = /* @__PURE__ */ newScriptHelper({});
+
+export function newScriptHelper(options: {
+  spawn?: SpawnOptions;
+  helper?: HelperOptions;
+}) {
+  const spawnOptions = { ...defaultSpawnOptions, ...options.spawn };
+  const helperOptions = options.helper ?? {};
+
+  return function $(strings: TemplateStringsArray, ...params: ScriptParam[]) {
+    const command = [zip(strings, params), strings.at(-1)].flat(2).join("");
+    if (helperOptions.verbose) {
+      console.log("$ ", command);
+    }
+    return new SpawnPromise(command, spawnOptions, helperOptions);
+  };
+}
+
+class SpawnPromise implements PromiseLike<string> {
+  child: ChildProcess;
+  promise: Promise<string>;
+  stderr: string = "";
+  stdout: string = "";
+
+  constructor(
+    private command: string,
+    private options: SpawnOptions,
+    private helperOptions: HelperOptions
+  ) {
+    const child = spawn(this.command, this.options);
+    this.child = child;
+    this.promise = new Promise<string>((resolve, reject) => {
+      // TODO: support stdin?
+      child.stdin;
+
+      child.stdout?.on("data", (raw: unknown) => {
+        processOutput(raw, (v) => (this.stdout += v), reject);
+      });
+
+      child.stderr?.on("data", (raw: unknown) => {
+        processOutput(raw, (v) => (this.stdout += v), reject);
+      });
+
+      child.on("close", (code) => {
+        if (code === 0) {
+          let stdout = this.stdout;
+          if (!this.helperOptions.noTrim) {
+            stdout = stdout.trim();
+          }
+          resolve(this.stdout.trim());
+        } else {
+          reject(new Error(`ChildProcess code ${code}`));
+        }
+      });
+
+      child.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+
+  // delegate promise api
+  then: PromiseLike<string>["then"] = (...args) => this.promise.then(...args);
+}
+
+function processOutput(
+  raw: unknown,
+  onSuccess: (v: string) => void,
+  onError: (v: unknown) => void
+) {
+  if (typeof raw === "string") {
+    return onSuccess(raw);
+  } else if (raw instanceof Buffer) {
+    return onSuccess(raw.toString());
+  }
+  onError(new Error("unknown data", { cause: raw }));
+}
