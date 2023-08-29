ba68d0d6ef012b8c6ca038a8810c1a5805e90bb1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-29 14:51:47 +0900

    chore: comment

--- packages/icheck-ts/vitest.config.ts
+++ packages/icheck-ts/vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     dir: "./src",
-    // for `process.chdir` in `setupTestFixture`
+    // worker thread doesn't support `process.chdir` in `setupTestFixture`.
+    // we see `ERR_IPC_CHANNEL_CLOSED` when exiting watch mode, but it looks okay otherwise.
     poolMatchGlobs: [["**/*", "child_process"]],
   },
 });
