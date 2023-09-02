2d31855a3fd88d1e4ee7fe484b188e2aec9e487e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-03 02:50:07 +0900

    chore(utils): fix PURE comment for colors

--- packages/utils/src/colors.ts
+++ packages/utils/src/colors.ts
@@ -16,14 +16,13 @@ const baseColors = [
 type BaseColor = (typeof baseColors)[number];
 type Color = BaseColor | `bg${Capitalize<BaseColor>}`;
 
-export const colors = /* @__PURE__ */ Object.fromEntries(
-  baseColors.flatMap((fg, i) => {
-    const bg = `bg${fg.slice(0, 1).toUpperCase()}${fg.slice(1)}`;
-    return [
-      // fg: 30..
-      // bg: 40..
-      [fg, (v: string) => `\u001B[${30 + i}m${v}\u001B[39m`],
-      [bg, (v: string) => `\u001B[${40 + i}m${v}\u001B[49m`],
-    ] as any;
-  })
-) as Record<Color, (v: string) => string>;
+export const colors = /* @__PURE__ */ (() =>
+  Object.fromEntries(
+    baseColors.flatMap((fg, i) => {
+      const bg = `bg${fg.slice(0, 1).toUpperCase()}${fg.slice(1)}`;
+      return [
+        [fg, (v: string) => `\u001B[${30 + i}m${v}\u001B[39m`],
+        [bg, (v: string) => `\u001B[${40 + i}m${v}\u001B[49m`],
+      ] as any;
+    })
+  ) as Record<Color, (v: string) => string>)();
