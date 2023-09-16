e953ca5ff0fd16cee373727484b3f62d2405d6d5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-16 12:09:54 +0900

    feat(utils): add `none<T>()`

--- packages/utils/src/index.test.ts
+++ packages/utils/src/index.test.ts
@@ -10,7 +10,7 @@ import {
   subscribeEventListenerFactory,
   typedBoolean,
 } from "./misc";
-import { mapOption } from "./option";
+import { mapOption, none } from "./option";
 import { newPromiseWithResolvers } from "./promise";
 import { escapeRegExp, mapRegExp, regExpRaw } from "./regexp";
 import {
@@ -488,6 +488,19 @@ describe(mapOption, () => {
       | number
       | undefined;
     expect(result).toMatchInlineSnapshot("3");
+  });
+});
+
+describe(none, () => {
+  it("basic", () => {
+    const v = {
+      email: "",
+      age: none<number>(),
+    } satisfies {
+      email: string;
+      age: number | undefined;
+    };
+    expect(v).toMatchInlineSnapshot();
   });
 });
 
