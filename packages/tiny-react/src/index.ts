94f44ad8ab36b6c8d1c009ac5a8f09acffeb1f11
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-03 13:16:07 +0900

    chore: export more types

--- packages/tiny-react/src/index.ts
+++ packages/tiny-react/src/index.ts
@@ -1,4 +1,5 @@
-export { Fragment } from "./helper/hyperscript";
-export { h } from "./helper/hyperscript";
 export { render } from "./reconciler";
+export type { VNode } from "./virtual-dom";
+export { Fragment, h } from "./helper/hyperscript";
+export type { ComponentChildren, ComponentChild } from "./helper/common";
 export * from "./hooks";
