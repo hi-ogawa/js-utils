e7006e9c782c44366b32996b3c6b7d4669b45b86
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-07 12:12:35 +0900

    refactor(tiny-store): simplify react import (#148)

--- packages/tiny-store/src/react.ts
+++ packages/tiny-store/src/react.ts
@@ -1,16 +1,16 @@
-import React from "react";
+import { useMemo, useSyncExternalStore } from "react";
 import type { TinyStoreApi } from "./core";
 import { createTinyStoreWithStorage } from "./local-storage";
 
 export function useTinyStore<T, RO extends boolean>(
   store: TinyStoreApi<T, RO>
 ): [T, TinyStoreApi<T, RO>["set"]] {
-  React.useSyncExternalStore(store.subscribe, store.get, store.get);
-  return [store.get(), store.set];
+  const value = useSyncExternalStore(store.subscribe, store.get, store.get);
+  return [value, store.set];
 }
 
 export function useTinyStoreStorage<T>(key: string, defaultValue: T) {
-  const store = React.useMemo(
+  const store = useMemo(
     () => createTinyStoreWithStorage(key, defaultValue),
     [key]
   );
