610c32a5ad48503da5f471092d002d5ce5469d62
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-07 11:33:40 +0900

    wip

--- packages/tiny-react/src/compat/index.ts
+++ packages/tiny-react/src/compat/index.ts
@@ -1,2 +1,47 @@
+// not really compat but maybe good enough for simple cases...
 // cf. https://github.com/preactjs/preact/blame/9c5a82efcc3dcbd0035c694817a3022d81264687/compat/src/index.js
-// TODO
+
+// probably critical missing features
+// - defaultValue prop
+// - dangerouslySetInnerHTML prop
+// - mutable ref prop
+// - forwardRef
+// - useLayoutEffect
+
+import {
+  Fragment,
+  render,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "../index";
+import { createRoot, useSyncExternalStore } from "./misc";
+
+export {
+  render,
+  Fragment,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  useSyncExternalStore,
+  createRoot,
+};
+
+export default {
+  render,
+  Fragment,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  useSyncExternalStore,
+  createRoot,
+};
