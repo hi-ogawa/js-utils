d170d5e8f262d958d8c9729281ef9709868964cb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-10 15:40:17 +0900

    chore: remove packages/tiny-refresh

--- packages/tiny-refresh/src/webpack.ts
+++
@@ -1,7 +0,0 @@
-import { hmrTransform } from "./transform";
-
-export default (source: string): string => {
-  return (
-    hmrTransform(source, { bundler: "webpack4", runtime: "react" }) ?? source
-  );
-};
