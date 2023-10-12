a58c29df161f24142f4908c0471dd40b07e7e2f8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-12 12:49:08 +0900

    chore: comment

--- packages/tiny-react/src/context.ts
+++ packages/tiny-react/src/context.ts
@@ -20,6 +20,10 @@ export function createContext<T>(defaultValue: T): Context<T> {
 }
 
 export function useContext<T>(context: Context<T>): T {
+  // we don't have to setup "force update"
+  // since if Provider.value changes, then it means Provider re-render all children
+  // including the one using this `useContext`
+  // unless we have "memo" component?
   return RenderContextManager.get(context);
 }
 
@@ -36,7 +40,8 @@ export class RenderContextManager {
 
   static push(key: ContextKey, value: unknown) {
     tinyassert(this.current);
-    // TODO: copy-on-write but O((number of contexts)^2) perf is probably not what react does.
+    // copy-on-write but O((number of contexts)^2) in total perf. it seems alright though
+    // https://github.com/preactjs/preact/blob/4b1a7e9276e04676b8d3f8a8257469e2f732e8d4/src/diff/index.js#L222-L224
     this.current = new Map(this.current);
     this.current.set(key, value);
   }
