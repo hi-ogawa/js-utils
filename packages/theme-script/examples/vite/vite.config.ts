cef3f5ec32d032caf87d6dbbc16c0544d813c82b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-14 11:10:19 +0900

    chore: revert theme-sscript

--- packages/theme-script/examples/vite/vite.config.ts
+++ packages/theme-script/examples/vite/vite.config.ts
@@ -1,12 +1,10 @@
 import { themeScriptPlugin } from "@hiogawa/theme-script/dist/vite";
-import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     unocss(),
-    react(),
     themeScriptPlugin({ storageKey: "theme-script-vite:theme" }),
   ],
   clearScreen: false,
