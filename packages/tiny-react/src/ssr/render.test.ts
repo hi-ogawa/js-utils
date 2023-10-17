eced71379b9df5e6035c5e2d7a164c9b8b76f350
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-17 15:21:30 +0900

    test: add todo

--- packages/tiny-react/src/ssr/render.test.ts
+++ packages/tiny-react/src/ssr/render.test.ts
@@ -44,4 +44,42 @@ describe(renderToString, () => {
       '"<div><span>{&quot;prop&quot;:123,&quot;state&quot;:&quot;init&quot;}</span></div>"'
     );
   });
+
+  //
+  // TODO
+  //
+
+  it("attribute-lowercase", () => {
+    // not kebab case but lower case
+    // cf. https://github.com/preactjs/preact-render-to-string/blob/ba4f4eb1f81e01ac15aef377ae609059e9b2ffce/src/util.js#L4
+    const vnode = h.input({
+      maxLength: 0,
+      readOnly: true,
+      inputMode: "numeric",
+    });
+    expect(renderToString(vnode)).toMatchInlineSnapshot(
+      '"<input max-length=\\"0\\" read-only=\\"true\\" input-mode=\\"numeric\\"/>"'
+    );
+  });
+
+  it("tag-textarea", () => {
+    // textarea value should be inside the tag
+    //   <textarea>hello</textarea>
+    const vnode = h.textarea({ value: "hello" });
+    expect(renderToString(vnode)).toMatchInlineSnapshot(
+      '"<textarea value=\\"hello\\"></textarea>"'
+    );
+  });
+
+  it("tag-select", () => {
+    // "selected" should be added to option
+    const vnode = h.select(
+      { value: "a" },
+      h.option({ value: "a" }, "x"),
+      h.option({ value: "b" }, "y")
+    );
+    expect(renderToString(vnode)).toMatchInlineSnapshot(
+      '"<select value=\\"a\\"><option value=\\"a\\">x</option><option value=\\"b\\">y</option></select>"'
+    );
+  });
 });
