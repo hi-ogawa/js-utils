fa42478649e9499aa0876bd96e6f6a9824f9735b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-28 08:53:46 +0900

    chore: comment

--- packages/tiny-refresh/tsup.config.ts
+++ packages/tiny-refresh/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/vite.ts", "src/webpack.ts"],
   format: ["esm", "cjs"],
-  // can be used for old webpack 4
+  // support old webpack 4
   target: "es5",
   dts: true,
   splitting: false,
