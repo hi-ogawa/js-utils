455bd7654183245c97834db417894c9d724c8334
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-01 14:13:44 +0900

    wip backtrack

--- packages/utils/src/edit-distance.test.ts
+++ packages/utils/src/edit-distance.test.ts
@@ -3,8 +3,55 @@ import { solveEditDistance } from "./edit-distance";
 
 describe(solveEditDistance, () => {
   it("basic", () => {
-    expect(
-      solveEditDistance([..."kitten"], [..."sitting"])
-    ).toMatchInlineSnapshot("3");
+    expect(solveEditDistance([..."kitten"], [..."sitting"]))
+      .toMatchInlineSnapshot(`
+        {
+          "cost": 3,
+          "path": [
+            {
+              "cost": 3,
+              "i": 6,
+              "j": 7,
+              "op": 0,
+            },
+            {
+              "cost": 3,
+              "i": 5,
+              "j": 6,
+              "op": 0,
+            },
+            {
+              "cost": 2,
+              "i": 4,
+              "j": 5,
+              "op": 0,
+            },
+            {
+              "cost": 2,
+              "i": 3,
+              "j": 4,
+              "op": 0,
+            },
+            {
+              "cost": 2,
+              "i": 2,
+              "j": 3,
+              "op": 0,
+            },
+            {
+              "cost": 2,
+              "i": 1,
+              "j": 2,
+              "op": 0,
+            },
+            {
+              "cost": 1,
+              "i": 0,
+              "j": 1,
+              "op": 1,
+            },
+          ],
+        }
+      `);
   });
 });
--- packages/utils/src/edit-distance.ts
+++ packages/utils/src/edit-distance.ts
@@ -1,5 +1,12 @@
 import { range } from "./lodash";
 
+const ops = {
+  none: -1,
+  replace: 0,
+  insert: 1,
+  remove: 2,
+} as const;
+
 export function solveEditDistance<T>(
   xs: T[],
   ys: T[],
@@ -7,29 +14,56 @@ export function solveEditDistance<T>(
 ) {
   const n = xs.length;
   const m = ys.length;
-  const dp = range(n + 1).map(() => range(m + 1).fill(0));
+  const dp = range(n + 1).map(() => range(m + 1).map(() => [0, ops.none]));
 
   for (const i of range(n)) {
-    dp[i + 1][0] = dp[i][0] + costFn(xs[i], undefined);
+    dp[i + 1][0][0] = dp[i][0][0] + costFn(xs[i], undefined);
+    dp[i + 1][0][1] = ops.remove;
   }
 
   for (const j of range(m)) {
-    dp[0][j + 1] = dp[0][j] + costFn(undefined, ys[j]);
+    dp[0][j + 1][0] = dp[0][j][0] + costFn(undefined, ys[j]);
+    dp[0][j + 1][1] = ops.insert;
   }
 
   for (const i of range(n)) {
     for (const j of range(m)) {
-      const replace = dp[i][j] + costFn(xs[i], ys[i]);
-      const insert = dp[i + 1][j] + costFn(undefined, ys[i]);
-      const remove = dp[i][j + 1] + costFn(xs[i], undefined);
+      const replace = dp[i][j][0] + costFn(xs[i], ys[i]);
+      const insert = dp[i + 1][j][0] + costFn(undefined, ys[i]);
+      const remove = dp[i][j + 1][0] + costFn(xs[i], undefined);
       const next = Math.min(replace, insert, remove);
-      // TODO: backtrack
-      // const op: 0 | 1 | 2 = next === replace ? 0 : next === insert ? 1 : 2;
-      dp[i + 1][j + 1] = next;
+      dp[i + 1][j + 1][0] = next;
+      dp[i + 1][j + 1][1] =
+        next === replace
+          ? ops.replace
+          : next === insert
+          ? ops.insert
+          : ops.remove;
     }
   }
 
-  return dp[n][m];
+  console.log(dp);
+
+  const cost = dp[n][m][0];
+
+  // backtrack
+  const path: any[] = [];
+  let i = n;
+  let j = m;
+  while (i > 0 || j > 0) {
+    const [cost, op] = dp[i][j];
+    path.push({ i, j, cost, op });
+    if (op === ops.replace) {
+      i--;
+      j--;
+    } else if (op === ops.insert) {
+      j--;
+    } else if (op === ops.remove) {
+      i--;
+    }
+  }
+
+  return { cost, path };
 }
 
 function defaultCostFn(x: unknown, y: unknown) {
