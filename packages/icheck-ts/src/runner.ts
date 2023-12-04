ab55dfa0fd3ed383d92f7ba7a943dfbac55e66c4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-04 18:19:48 +0900

    chore: minor

--- packages/icheck-ts/src/runner.ts
+++ packages/icheck-ts/src/runner.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import * as process from "node:process";
+import process from "node:process";
 import { fileURLToPath, pathToFileURL } from "node:url";
 import {
   DefaultMap,
