a80cd96a0ec0d714eea9c076abc1442c9c87e449
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-24 12:29:03 +0900

    chore: tweak test

--- packages/utils-node/src/prompt.test.ts
+++ packages/utils-node/src/prompt.test.ts
@@ -1,4 +1,4 @@
-import { tinyassert } from "@hiogawa/utils";
+import { sleep, tinyassert } from "@hiogawa/utils";
 import { describe, expect, it } from "vitest";
 import { promptQuestion } from "./prompt";
 import { $ } from "./script";
@@ -7,7 +7,24 @@ describe(promptQuestion, () => {
   it("basic", async () => {
     const proc = $`pnpm exec tsx ./src/fixtures/prompt-basic.ts`;
     tinyassert(proc.child.stdin);
+
+    await retry(() => tinyassert(proc.stdout.includes("hello?")));
+    expect(proc.stdout).toMatchInlineSnapshot(`"hello? "`);
+
     proc.child.stdin.end("hi\n");
-    expect(await proc.promise).toMatchInlineSnapshot(`"hello? answer: hi"`);
+    expect(await proc).toMatchInlineSnapshot(`"hello? answer: hi"`);
   });
 });
+
+async function retry<T>(f: () => T): Promise<Awaited<T>> {
+  let error: unknown;
+  for (const interval of [100, 250, 500, 1000]) {
+    try {
+      return await f();
+    } catch (e) {
+      error = e;
+    }
+    await sleep(interval);
+  }
+  throw error;
+}
