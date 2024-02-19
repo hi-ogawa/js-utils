47409b282fcf90eae362d98bdbab49e5cf27d996
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-19 09:31:18 +0900

    fix: export

--- packages/tiny-rpc/src/index.ts
+++ packages/tiny-rpc/src/index.ts
@@ -3,3 +3,8 @@ export * from "./adapter-http";
 export * from "./adapter-message-port";
 export * from "./adapter-message-port-node";
 export * from "./validation";
+export {
+  TwoWaySseClient,
+  TwoWaySseClientProxy,
+  createTwoWaySseHandler,
+} from "./server-sent-event";
