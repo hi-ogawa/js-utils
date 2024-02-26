760f9f4ef05b678da9f1f5c751fa3533b82d4c0b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-26 18:31:47 +0900

    feat: support custom replacer/reviver

--- packages/json-extra/src/reference.test.ts
+++ packages/json-extra/src/reference.test.ts
@@ -161,7 +161,56 @@ describe("reference", () => {
     expect(u).toBe(u[1].hi);
   });
 
-  it("collision", () => {
+  it("collision 1", () => {
+    const v1 = { hi: 0 };
+    const v2 = ["!", v1];
+    const v = [v1, v2, v2];
+    const replaced = replaceReference(v);
+    expect(replaced).toMatchInlineSnapshot(`
+      [
+        {
+          "hi": 0,
+        },
+        [
+          "!",
+          "!",
+          [
+            "!",
+            1,
+          ],
+        ],
+        [
+          "!",
+          2,
+        ],
+      ]
+    `);
+    const u = reviveReference(replaced) as any;
+    expect(u).toMatchInlineSnapshot(`
+      [
+        {
+          "hi": 0,
+        },
+        [
+          "!",
+          {
+            "hi": 0,
+          },
+        ],
+        [
+          "!",
+          {
+            "hi": 0,
+          },
+        ],
+      ]
+    `);
+    expect(u).toEqual(v);
+    expect(u[0]).toBe(u[1][1]);
+    expect(u[1]).toBe(u[2]);
+  });
+
+  it("collision 2", () => {
     const v1 = { hi: 0 };
     const v2 = ["!", v1];
     const v = [
@@ -270,5 +319,195 @@ describe("reference", () => {
     expect(u[0]).toBe(u[1][0]);
     expect(u[0]).toBe(u[3][1]);
     expect(u[0]).toBe(u[4][1]["!"]);
+  });
+
+  it("custom single", () => {
+    const v1 = new Date("2000-01-01T00:00:00Z");
+    const v = [v1, [v1], { foo: v1 }];
+    const replaced = replaceReference(v);
+    expect(replaced).toMatchInlineSnapshot(`
+      [
+        [
+          "!Date",
+          "2000-01-01T00:00:00.000Z",
+        ],
+        [
+          [
+            "!",
+            1,
+          ],
+        ],
+        {
+          "foo": [
+            "!",
+            1,
+          ],
+        },
+      ]
+    `);
+    const u = reviveReference(replaced) as any;
+    expect(u).toMatchInlineSnapshot(`
+      [
+        2000-01-01T00:00:00.000Z,
+        [
+          2000-01-01T00:00:00.000Z,
+        ],
+        {
+          "foo": 2000-01-01T00:00:00.000Z,
+        },
+      ]
+    `);
+    expect(u).toEqual(v);
+    expect(u[0]).toBe(u[1][0]);
+    expect(u[0]).toBe(u[2].foo);
+  });
+
+  it("custom constant", () => {
+    const v1 = [undefined, Infinity, NaN];
+    const v = [v1, [v1]];
+    const replaced = replaceReference(v);
+    expect(replaced).toMatchInlineSnapshot(`
+      [
+        [
+          [
+            "!undefined",
+            0,
+          ],
+          [
+            "!Infinity",
+            0,
+          ],
+          [
+            "!NaN",
+            0,
+          ],
+        ],
+        [
+          [
+            "!",
+            1,
+          ],
+        ],
+      ]
+    `);
+    const u = reviveReference(replaced) as any;
+    expect(u).toMatchInlineSnapshot(`
+      [
+        [
+          undefined,
+          Infinity,
+          NaN,
+        ],
+        [
+          [
+            undefined,
+            Infinity,
+            NaN,
+          ],
+        ],
+      ]
+    `);
+    expect(u).toEqual(v);
+    expect(u[0]).toBe(u[1][0]);
+  });
+
+  it("custom container 1", () => {
+    const v1 = new Date("2000-01-01T00:00:00Z");
+    const v2 = new Set([v1, { foo: v1 }]);
+    const v = [v2, [v2]];
+    const replaced = replaceReference(v);
+    expect(replaced).toMatchInlineSnapshot(`
+      [
+        [
+          "!Set",
+          [
+            [
+              "!Date",
+              "2000-01-01T00:00:00.000Z",
+            ],
+            {
+              "foo": [
+                "!",
+                3,
+              ],
+            },
+          ],
+        ],
+        [
+          [
+            "!",
+            1,
+          ],
+        ],
+      ]
+    `);
+    const u = reviveReference(replaced) as any;
+    expect(u).toMatchInlineSnapshot(`
+      [
+        Set {
+          2000-01-01T00:00:00.000Z,
+          {
+            "foo": 2000-01-01T00:00:00.000Z,
+          },
+        },
+        [
+          Set {
+            2000-01-01T00:00:00.000Z,
+            {
+              "foo": 2000-01-01T00:00:00.000Z,
+            },
+          },
+        ],
+      ]
+    `);
+    expect(u).toEqual(v);
+    expect(u[0]).toBe(u[1][0]);
+    const [x, y] = [...u[0]];
+    expect(x).toBe(y.foo);
+  });
+
+  it("custom container cyclic", () => {
+    const v1 = new Set(["hi" as any]);
+    v1.add(v1);
+    const v = [v1, [v1]];
+    const replaced = replaceReference(v);
+    expect(replaced).toMatchInlineSnapshot(`
+      [
+        [
+          "!Set",
+          [
+            "hi",
+            [
+              "!",
+              1,
+            ],
+          ],
+        ],
+        [
+          [
+            "!",
+            1,
+          ],
+        ],
+      ]
+    `);
+    const u = reviveReference(replaced) as any;
+    expect(u).toMatchInlineSnapshot(`
+      [
+        Set {
+          "hi",
+          [Circular],
+        },
+        [
+          Set {
+            "hi",
+            [Circular],
+          },
+        ],
+      ]
+    `);
+    expect(u).toEqual(v);
+    expect(u[0]).toBe(u[1][0]);
+    expect(u[0]).toBe([...u[0]][1]);
   });
 });
--- packages/json-extra/src/reference.ts
+++ packages/json-extra/src/reference.ts
@@ -1,8 +1,10 @@
-// TODO
-// - support custom replacer/revivor plugin
+import { tinyassert } from "@hiogawa/utils";
 
 export function replaceReference(data: unknown) {
   const refs = new Map<unknown, number>();
+
+  // TODO: configurable
+  const plugins: Record<string, Plugin<any>> = builtinPlugins;
 
   function recurse(v: unknown) {
     // return reference placeholder
@@ -20,11 +22,17 @@ export function replaceReference(data: unknown) {
       v = ["!", ...v];
     }
 
-    // TODO: custom replacer
-    // v = replacer(v)
+    // custom replacer
+    for (const tag in plugins) {
+      const p = plugins[tag];
+      if (p.is(v)) {
+        v = [`!${tag}`, p.replacer(v)];
+        break;
+      }
+    }
 
     if (v && typeof v === "object") {
-      v = Array.isArray(v) ? [...v] : { ...v };
+      v = shallowCopy(v);
       for (const [k, e] of Object.entries(v as any)) {
         (v as any)[k] = recurse(e);
       }
@@ -38,32 +46,131 @@ export function replaceReference(data: unknown) {
 export function reviveReference(data: unknown) {
   const refs: unknown[] = [];
 
+  // TODO: configurable
+  const plugins: Record<string, Plugin<any>> = builtinPlugins;
+
   function recurse(v: unknown) {
-    // replace reference placeholder
+    // revive reference placeholder
     if (Array.isArray(v) && v.length === 2 && v[0] === "!") {
       return refs[v[1]];
     }
 
+    let plugin: Plugin<any> | undefined;
+    let ref: unknown | undefined;
+
+    // TODO: refactor messy branches...
+
     // unescape custom encoding collision
     if (Array.isArray(v) && v.length >= 3 && v[0] === "!") {
       v = v.slice(1);
+      refs.push(v);
+    } else if (
+      // custom revivor to obtain ref before recurse
+      Array.isArray(v) &&
+      v.length === 2 &&
+      typeof v[0] === "string" &&
+      v[0][0] === "!"
+    ) {
+      const tag = v[0].slice(1);
+      v = shallowCopy(v[1]);
+      plugin = plugins[tag];
+      tinyassert(plugin);
+      ref = plugin.reviverRef();
+      refs.push(ref);
+      refs.push("__dummy"); // dummy ref to align with replaceReference's offset (TODO: probably should fix replaceReference side)
+    } else if (v && typeof v === "object") {
+      // track ref before recurse to handle circular references
+      v = shallowCopy(v);
+      refs.push(v);
     }
 
     if (v && typeof v === "object") {
-      v = Array.isArray(v) ? [...v] : { ...v };
-      // need to keep ref before recurse to handle circular reference
-      // TOOD: however this approach cannot work with custom reviver?
-      refs.push(v);
       for (const [k, e] of Object.entries(v as any)) {
         (v as any)[k] = recurse(e);
       }
     }
 
-    // TODO: custom reviver
-    // v = replacer(v)
+    // custom revivor after recurse
+    if (plugin) {
+      v = plugin.reviver(v, ref);
+    }
 
     return v;
   }
 
   return recurse(data);
 }
+
+// shallow copy to avoid overwriting original input
+function shallowCopy(v: unknown) {
+  if (v && typeof v === "object") {
+    return Array.isArray(v) ? [...v] : { ...v };
+  }
+  return v;
+}
+
+//
+// plugin
+//
+
+type Plugin<T> = {
+  is: (v: unknown) => boolean;
+  replacer: (v: T) => unknown;
+  reviver: (v: unknown, ref: T) => T;
+  reviverRef: () => T;
+};
+
+// type helper
+export function definePlugin<T>(v: Plugin<T>): Plugin<T> {
+  return v;
+}
+
+function definePluginConstant<T>(c: T): Plugin<T> {
+  return {
+    is: (v) => Object.is(v, c),
+    replacer: () => 0,
+    reviver: () => c,
+    reviverRef: () => c,
+  };
+}
+
+export const builtinPlugins = {
+  //
+  // constants
+  //
+  undefined: definePluginConstant(undefined),
+  Infinity: definePluginConstant(Infinity),
+  "-Infinity": definePluginConstant(-Infinity),
+  NaN: definePluginConstant(NaN),
+  "-0": definePluginConstant(-0),
+
+  //
+  // non containers
+  //
+  Date: definePlugin<Date>({
+    is: (v) => v instanceof Date,
+    replacer: (v) => v.toISOString(),
+    reviver: (v, ref) => {
+      tinyassert(typeof v === "string");
+      ref.setTime(new Date(v).getTime());
+      return ref;
+    },
+    reviverRef: () => new Date(0),
+  }),
+
+  //
+  // containers
+  //
+  Set: definePlugin<Set<unknown>>({
+    is: (v) => v instanceof Set,
+    replacer: (v) => Array.from(v),
+    reviver: (v, ref) => {
+      tinyassert(Array.isArray(v));
+      for (const e of v) {
+        ref.add(e);
+      }
+      return ref;
+    },
+    reviverRef: () => new Set(),
+  }),
+};
