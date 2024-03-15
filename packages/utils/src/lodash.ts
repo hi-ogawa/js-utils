0df7dbd94bce630822628a2f1daf0dc8bbd98d54
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-15 15:26:37 +0900

    fix(utils): fix this context in `memoize` (#216)

--- packages/utils/src/lodash.ts
+++ packages/utils/src/lodash.ts
@@ -161,16 +161,15 @@ export function isNotNil<T>(value: T): value is NonNullable<T> {
 
 // TODO: handle error?
 export function once<F extends (...args: any[]) => any>(f: F): F {
-  let result: unknown;
+  let result: ReturnType<F>;
   let called = false;
-  function wrapper(...args: any[]) {
+  return safeFunctionCast<F>(function (this: unknown, ...args) {
     if (!called) {
-      result = f(...args);
+      result = f.apply(this, args);
       called = true;
     }
     return result;
-  }
-  return wrapper as F;
+  });
 }
 
 export function memoize<F extends (...args: any[]) => any>(
@@ -186,7 +185,7 @@ export function memoize<F extends (...args: any[]) => any>(
   // by default, use 1st argument as a cache key which is same as lodash
   const keyFn = options?.keyFn ?? ((...args) => args[0]);
   const cache = options?.cache ?? new Map<unknown, ReturnType<F>>();
-  return safeFunctionCast<F>((...args) => {
+  return safeFunctionCast<F>(function (this: unknown, ...args) {
     const key = keyFn(...args);
     // avoid `has/get` since they might not be atomic for some cache (e.g. ttl cache).
     // however, this logic means `undefined` value will not be cached.
@@ -194,7 +193,7 @@ export function memoize<F extends (...args: any[]) => any>(
     if (typeof value !== "undefined") {
       return value;
     }
-    const newValue = f(...args);
+    const newValue = f.apply(this, args);
     cache.set(key, newValue);
     return newValue;
   });
