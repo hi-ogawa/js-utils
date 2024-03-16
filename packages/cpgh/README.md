eab5e9beac8d8b5c5d213004eaff59fb7d10d294
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-16 14:02:31 +0900

    wip

--- packages/cpgh/README.md
+++ packages/cpgh/README.md
@@ -3,8 +3,5 @@
 cli to copy github repo, similart to [degit](https://github.com/Rich-Harris/degit)
 
 ```sh
-npx cpgh https://github.com/hi-ogawa/vite-plugins/tree/main/packages/react-server/examples/starter my-app
-
-#
-npx cpgh https://github.com/hi-ogawa/vite-plugins --branch main --dir packages/react-server/examples/starter my-app
+npx cpgh https://github.com/vitest-dev/vitest/tree/main/examples/basic my-app
 ```
--- packages/cpgh/src/cli.ts
+++ packages/cpgh/src/cli.ts
@@ -1,12 +1,17 @@
+import { exec } from "node:child_process";
 import fs from "node:fs";
+import path from "node:path";
 import process from "node:process";
+import { promisify } from "node:util";
 import { name, version } from "../package.json";
+
+const execp = promisify(exec);
 
 const HELP = `\
 ${name}@${version}
 
 Usage:
-  npx cpgh https://github.com/<user>/<repo>/tree/<commit>/<subdir> <outdir>
+  npx cpgh https://github.com/<user>/<repo>/tree/<branch>/<subdir> <outdir>
 
 Examples:
   npx cpgh https://github.com/vitest-dev/vitest/tree/main/examples/basic my-app
@@ -20,40 +25,45 @@ async function main() {
     if (args.includes("-h") || args.includes("--help") || args.length === 0) {
       return;
     }
-    return 1;
+    process.exit(1);
   }
 
   // TODO: decode url?
-  const [url, outdir] = args;
+  const [url, outDir] = args;
   const match = url.match(
     new RegExp(String.raw`^https://github.com/([^/]+)/([^/]+)/tree/(.*)$`)
   );
   if (!match) {
     console.log(HELP);
-    return 1;
+    process.exit(1);
   }
   const [, user, repo, rest] = match;
-  let commit = rest;
-  let subdir = "";
+  let branch: string;
+  let subDir: string;
   if (rest.includes("[/]")) {
-    [commit, subdir] = rest.split("[/]");
+    [branch, subDir] = rest.split("[/]");
   } else {
     const splits = rest.split("/");
-    commit = splits[0];
-    subdir = splits.slice(1).join("/");
+    branch = splits[0];
+    subDir = splits.slice(1).join("/");
+  }
+  subDir ||= "/";
+
+  // TODO: --force
+  if (fs.existsSync(outDir)) {
+    console.log(`'${outDir}' already exists. use --force to overwrite it.`);
+    process.exit(1);
   }
 
-  // TODO
-  console.log({
-    user,
-    repo,
-    commit,
-    subdir,
-    outdir,
-  });
-  fs;
-
-  return;
+  const tmpDir = path.join(outDir, ".tmp-cpgh");
+  await fs.promises.mkdir(tmpDir, { recursive: true });
+  await execp(
+    `git clone --sparse --no-checkout --filter=tree:0 --depth 1 https://github.com/${user}/${repo} --branch ${branch} ${tmpDir}`
+  );
+  await execp(`git -C ${tmpDir} sparse-checkout add ${subDir}`);
+  await execp(`git -C ${tmpDir} checkout`);
+  await fs.promises.cp(path.join(tmpDir, subDir), outDir, { recursive: true });
+  await fs.promises.rm(tmpDir, { recursive: true, force: true });
 }
 
 main();
