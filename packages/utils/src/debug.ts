f9f76e21b6cb53531c4a572d3790e381a856b3ff
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-25 09:58:01 +0900

    fix: use console.debug on browser

--- packages/utils/src/debug.ts
+++ packages/utils/src/debug.ts
@@ -10,13 +10,19 @@ export function createDebug(namespace: string) {
       (globalThis as any).process?.env?.["DEBUG"] ??
       (globalThis as any).__DEBUG;
     if (typeof flag === "string" && pattern.test(flag)) {
-      console.error(bold(`⊳ ${namespace}`), ...args);
+      let prefix = `⊳ ${namespace}`;
+      const stderr = (globalThis as any)?.process?.stderr;
+      if (stderr) {
+        if (stderr?.isTTY) {
+          prefix = `\u001B[1m${prefix}\u001B[22m`;
+        }
+        console.error(prefix, ...args);
+      } else {
+        console.debug(prefix, ...args);
+      }
     }
   };
 }
-
-const bold = (v: string) =>
-  (globalThis as any)?.process?.stderr?.isTTY ? `\u001B[1m${v}\u001B[22m` : v;
 
 export function createDebugPattern(namespace: string) {
   const parts = namespace.split(":");
