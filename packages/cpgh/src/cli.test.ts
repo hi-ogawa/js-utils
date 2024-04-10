f5abfb9041e5da9b5b253a9ca89e3118ab8b3b8b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-10 09:52:05 +0900

    ci: update node to fix tests (#221)

--- packages/cpgh/src/cli.test.ts
+++ packages/cpgh/src/cli.test.ts
@@ -1,7 +1,7 @@
 import { $ } from "@hiogawa/utils-node";
 import { expect, it } from "vitest";
 
-it("basic", async () => {
+it("basic", { timeout: 60_000 }, async () => {
   await $`pnpm cli --force https://github.com/vitest-dev/vitest/tree/main/examples/basic /tmp/my-project`;
   const output = await $`ls /tmp/my-project`;
   expect(output.split(/\s/).sort()).toMatchInlineSnapshot(`
