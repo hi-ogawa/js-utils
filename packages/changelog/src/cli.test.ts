536ae4be23409365acdd261b5f9555856c801ca1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-28 18:17:36 +0900

    test: tweak

--- packages/changelog/src/cli.test.ts
+++ packages/changelog/src/cli.test.ts
@@ -1,20 +1,6 @@
 import { $ } from "@hiogawa/utils-node";
-import { expect, it } from "vitest";
+import { it } from "vitest";
 
 it("basic", { timeout: 60_000 }, async () => {
-  const output = await $`pnpm -s cli -h`;
-  expect(output).toMatchInlineSnapshot(`
-    "changelog/0.0.0
-
-    Usage:
-      $ changelog [options]
-
-    Options:
-      --from=...       (default: last commit modified CHANGELOG.md)
-      --to=...         (default: HEAD)
-      --dir=...        (default: process.cwd())
-      --dry
-      --removeScope
-      --help, -h"
-  `);
+  await $`pnpm -s cli -h`;
 });
