4d2aac52b1d2e4971723820f9a54da8ab52a22b8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-21 17:44:56 +0900

    wip: loader

--- packages/tiny-refresh/examples/react/webpack/loader.js
+++ packages/tiny-refresh/examples/react/webpack/loader.js
@@ -1,7 +1,14 @@
+import { transformWebpack } from "@hiogawa/tiny-refresh/transform";
+
 /**
  * @type {import("webpack").LoaderDefinitionFunction<{}, {}>}
  */
-export default function loader(input) {
-  // TODO
-  return input;
+export default async function loader(input) {
+  const callback = this.async();
+  const result = await transformWebpack(input, {
+    refreshRuntime: "@hiogawa/tiny-refresh",
+    runtime: "react",
+    debug: true,
+  });
+  callback(null, result ?? input);
 }
