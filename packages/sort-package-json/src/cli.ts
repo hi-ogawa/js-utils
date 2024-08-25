e90b5e883aa2017df8e3a8c66ace99ddfedc4093
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-25 18:04:30 +0900

    feat(sort-package-json): sort dependencies (#267)

--- packages/sort-package-json/src/cli.ts
+++ packages/sort-package-json/src/cli.ts
@@ -14,7 +14,7 @@ Usage:
   sort-package-json [package.json files...]
 
 Examples:
-  # Sort package.json files in pnpm workspace
+  # Sort all package.json files in pnpm workspace
   sort-package-json $(pnpm ls --filter '*' --depth -1 --json | jq -r '.[] | .path' | xargs -I '{}' echo '{}/package.json')
 `;
 
@@ -36,19 +36,47 @@ function main() {
 
 function sortPackageJson(infile: string): boolean {
   const original = JSON.parse(fs.readFileSync(infile, "utf-8"));
+
+  // sort top level keys
   const sorted = Object.fromEntries(
     Object.entries(original).sort(
       ([k1], [k2]) => (RULE_MAP[k1] ?? 1e8) - (RULE_MAP[k2] ?? 1e8)
     )
   );
-  if (
-    JSON.stringify(Object.keys(original)) ===
-    JSON.stringify(Object.keys(sorted))
-  ) {
-    return false;
+  let changed = !isEqual(Object.keys(original), Object.keys(sorted));
+
+  // sort dependencies
+  for (const depKey of [
+    "dependencies",
+    "devDependencies",
+    "dependenciesMeta",
+    "peerDependencies",
+    "peerDependenciesMeta",
+    "optionalDependencies",
+    "bundledDependencies",
+    "bundleDependencies",
+  ]) {
+    const deps = sorted[depKey];
+    if (deps && typeof deps === "object") {
+      sorted[depKey] = Object.fromEntries(
+        Object.entries(deps).sort(([k1], [k2]) => k1.localeCompare(k2))
+      );
+      changed ||= !isEqual(
+        Object.keys(deps),
+        Object.keys(sorted[depKey] as any)
+      );
+    }
   }
-  fs.writeFileSync(infile, JSON.stringify(sorted, null, 2) + "\n");
-  return true;
+
+  // write if changed
+  if (changed) {
+    fs.writeFileSync(infile, JSON.stringify(sorted, null, 2) + "\n");
+  }
+  return changed;
+}
+
+function isEqual(x: object, y: object) {
+  return JSON.stringify(x) === JSON.stringify(y);
 }
 
 main();
