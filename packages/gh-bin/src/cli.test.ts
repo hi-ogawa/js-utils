f614ebc1b1b4f8add8d840731d59d9c3f02bf438
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-17 16:08:47 +0900

    test: wip

--- packages/gh-bin/src/cli.test.ts
+++ packages/gh-bin/src/cli.test.ts
@@ -1,17 +1,6 @@
 import { $ } from "@hiogawa/utils-node";
-import { expect, it } from "vitest";
+import { it } from "vitest";
 
-it("basic", { timeout: 60_000 }, async () => {
-  await $`pnpm cli --force https://github.com/vitest-dev/vitest/tree/main/examples/basic /tmp/my-project`;
-  const output = await $`ls /tmp/my-project`;
-  expect(output.split(/\s/).sort()).toMatchInlineSnapshot(`
-    [
-      "README.md",
-      "package.json",
-      "src",
-      "test",
-      "tsconfig.json",
-      "vite.config.ts",
-    ]
-  `);
+it("basic", async () => {
+  await $`pnpm cli -h`;
 });
